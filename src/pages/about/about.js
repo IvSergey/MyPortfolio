4241374619145bdd415514003631288a4d8710ec
IvSergey <imas.spb@mail.ru>
2018-03-23 16:06:45 +0300

    svg fonts commit

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -4,17 +4,12 @@ import 'normalize.css';
 import '../../img/sprite/icons/vk.svg';
 import '../../img/sprite/icons/in.svg';
 import '../../img/sprite/icons/git.svg';
+import '../../img/sprite/icons/arrow.svg';
+import '../../img/sprite/icons/hamburger.svg';
 
 
-import '../../fonts/Roboto/Roboto-Bold.ttf';
-import '../../fonts/Roboto/Roboto-BoldItalic.ttf';
-import '../../fonts/Roboto/Roboto-Italic.ttf';
-import '../../fonts/Roboto/Roboto-Light.ttf';
-import '../../fonts/Roboto/Roboto-lightItalic.ttf';
-import '../../fonts/Roboto/Roboto-Medium.ttf';
-import '../../fonts/Roboto/Roboto-MediumItalic.ttf';
-import '../../fonts/Roboto/Roboto-Regular.ttf';
-import '../../fonts/Sansus-Webissimo-Regular/Sansus-Webissimo-Regular.ttf';
+
+import '../../fonts/Sansus-Webissimo-Regular/Sansus Webissimo-Regular.ttf';
 import '../../fonts/FontAwesome/fontawesome-webfont.ttf';
 
 
